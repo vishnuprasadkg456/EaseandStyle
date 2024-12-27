50eeab6d264169695a7980295d523e39c25044c6
vishnuprasadkg456 <vishnukg54@gmail.com>
2024-12-27 21:29:09 +0530

    implementation of add category ,add categoryoffer, remove categoryoffer

--- controller/admin/categoryController.js
+++ controller/admin/categoryController.js
@@ -1,4 +1,5 @@
 const Category = require("../../model/categorySchema");
+const Product = require("../../model/productSchema");
 
 
 
@@ -27,7 +28,7 @@ const categoryInfo = async (req,res)=>{
         res.redirect("/pageerror");
     }
 }
-
+//addCategory`
 const addCategory = async (req,res)=>{
     const {name,description} = req.body;
     try {
@@ -46,7 +47,70 @@ const addCategory = async (req,res)=>{
     }
 }
 
+//addCategoryOffer
+
+const addCategoryOffer = async (req,res)=>{
+    const {name,description,offer} = req.body;
+    try {
+        const percentage = parseInt(req.body.percentage);
+        const categoryId = req.body.categoryId;
+        const category = await Category.findById(categoryId);
+        if(!category){
+            return res.status(400).json({error:"Category not found"});
+        }
+        const products = await Product.find({category:categoryId});
+        const hasProductOffer = products.some((product)=>product.productOffer>percentage);
+        if(hasProductOffer){
+            return res.status(400).json({error:"Products within this category already have a product offer"});
+        }
+        await Category.updateOne({_id:categoryId},{$set:{categoryOffer:percentage}});
+
+        for(const product of products){
+            product.productOffer = 0;
+            product.salePrice = product.regularPrice;
+            await product.save();
+
+        }
+
+        res.json({status:true});
+
+    } catch (error) {
+        return res.status(404).json({status:false,message:"Internal Server Error"});
+    }
+}
+
+//removeCategoryOffer
+const removeCategoryOffer = async (req,res)=>{
+    try {
+        const categoryId = req.body.categoryId;
+        const category = await Category.findById(categoryId);
+        if(!category){
+            return res.status(404).json({status:false,message:"Category not found"});
+        }
+        const percentage = category.categoryOffer;
+        const products = await Product.find({category: category._id});
+
+        if(products.length>0){
+            for(const product of products){
+                
+                product.salePrice += Math.floor(product.regularPrice*(percentage/100));
+                product.productOffer = 0;
+                await product.save();
+            }
+        }
+
+        category.categoryOffer = 0;
+        await category.save();
+        res.json({status:true});
+    } catch (error) {
+        res.status(500).json({status:false,message:"Internal Server Error"});
+        
+    }
+}
+
 module.exports = {
     categoryInfo,
-    addCategory
+    addCategory,
+    addCategoryOffer,
+    removeCategoryOffer
 }
--- routes/admin/adminRoute.js
+++ routes/admin/adminRoute.js
@@ -21,6 +21,8 @@ router.get("/unblockCustomer",adminAuth,customerController.customerunBlocked);
 
 //category management
 router.get("/category",adminAuth,categoryController.categoryInfo);
-router.get("/addCategory",adminAuth,categoryController.addCategory)
+router.post("/addCategory",adminAuth,categoryController.addCategory)
+router.post("/addCategoryOffer",adminAuth,categoryController.addCategoryOffer);
+router.post("/removeCategoryOffer",adminAuth,categoryController.removeCategoryOffer);
 
 module.exports = router;
