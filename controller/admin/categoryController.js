379bf6807612b0e931b3dfd0b8649be57f203051
vishnuprasadkg456 <vishnukg54@gmail.com>
2024-12-28 00:29:25 +0530

    implementation of list category,unlist category

--- controller/admin/categoryController.js
+++ controller/admin/categoryController.js
@@ -108,9 +108,34 @@ const removeCategoryOffer = async (req,res)=>{
     }
 }
 
+//listCategory
+const getListCategory = async (req,res)=>{
+   try {
+    let id = req.query.id;
+    await Category.updateOne({_id:id},{$set:{isListed:false}});
+    res.redirect("/admin/category");
+   } catch (error) {
+    res.redirect("/pageerror");
+   }
+
+}
+
+//unlistCategory
+
+const getUnlistCategory = async (req,res)=>{
+ try {
+    let id = req.query.id;
+    await Category.updateOne({_id:id},{$set:{isListed:true}});
+    res.redirect("/admin/category");
+ } catch (error) {
+    res.redirect("/pageerror");
+ }   
+}
 module.exports = {
     categoryInfo,
     addCategory,
     addCategoryOffer,
-    removeCategoryOffer
+    removeCategoryOffer,
+    getListCategory,
+    getUnlistCategory
 }
--- routes/admin/adminRoute.js
+++ routes/admin/adminRoute.js
@@ -24,5 +24,7 @@ router.get("/category",adminAuth,categoryController.categoryInfo);
 router.post("/addCategory",adminAuth,categoryController.addCategory)
 router.post("/addCategoryOffer",adminAuth,categoryController.addCategoryOffer);
 router.post("/removeCategoryOffer",adminAuth,categoryController.removeCategoryOffer);
+router.get("/listCategory",adminAuth,categoryController.getListCategory);
+router.get("/unlistCategory",adminAuth,categoryController.getUnlistCategory);
 
 module.exports = router;
