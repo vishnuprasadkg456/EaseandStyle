6af2ab3807e20ce258df33fe5e68d23c19bb643c
vishnuprasadkg456 <vishnukg54@gmail.com>
2025-01-16 15:15:09 +0530

    default addresss featrue completed

--- controller/user/profileController.js
+++ controller/user/profileController.js
@@ -566,7 +566,7 @@ const setDefaultAddress = async (req, res) => {
             return res.status(404).json({ message: "Address not found or not updated" });
         }
 
-        res.status(200).json({ message: "Default address updated successfully" });
+        res.status(200).json({ success:true, message: "Default address updated successfully" });
     } catch (error) {
         console.error("Error setting default address:", error);
         res.status(500).json({ message: "Internal server error" });
